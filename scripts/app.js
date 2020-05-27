fbeea5ce6d600e0e14b1f493da3c8174f9caa174
obasemola <obasemolaayodele@gmail.com>
2020-05-27 17:17:16 +0300

    updating weather img with ternary operator

--- scripts/app.js
+++ scripts/app.js
@@ -25,12 +25,15 @@ const updateUI = (data) => {
     icon.setAttribute('src', iconSrc);
 
 
-    let timeSrc = null;
-    if(weather.IsDayTime){
-        timeSrc = 'img/day.svg';
-    } else {
-        timeSrc = 'img/night.svg';
-    };
+    //ternary operator
+    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+
+    // let timeSrc = null;
+    // if(weather.IsDayTime){
+    //     timeSrc = 'img/day.svg';
+    // } else {
+    //     timeSrc = 'img/night.svg';
+    // };
 
     time.setAttribute('src', timeSrc);
 
